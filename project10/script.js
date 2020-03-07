1448a3c3201ad350a2b11cad05e3af87303647e3
devalexolthoff <alexolthoff@tutanota.com>
2020-03-07 16:35:45 +0800

    Added a transitions and transfromation

--- project10/script.js
+++ project10/script.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     var banner = document.querySelector('main')
+    var logo = document.querySelector("#logo")
     setTimeout(function(){
         banner.className = "visible"
     }, 2000)
-})
-banner.addEventListener("mouseover", ()=>{
-    banner.className = "over"
-})
-
-banner.addEventListener("mouseout", ()=>{
-    banner.className = "visible"
+    banner.addEventListener("mouseover", ()=>{
+        banner.className = "over"
+        logo.className = "upsidedown"
+    })
+    
+    banner.addEventListener("mouseout", ()=>{
+        banner.className = "visible"
+        logo.className = "";
+    })
 })
